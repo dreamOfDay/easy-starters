2cbdae7256a282dc5b3e1f7f30a44aad44464515
yuj <nosae_reverse@163.com>
2021-12-09 22:21:22 +0800

    add mongo-starter

---
+++ mongo-starter/src/main/java/com/lx/mongo/MongoAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.lx.mongo;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: yuj
+ * @Date: 2021/12/9
+ * @Describe:
+ */
+@Configuration
+public class MongoAutoConfiguration {
+}
