3d311b5bfa05fa6b2f24237a99e2182fccc187aa
yuj <nosae_reverse@163.com>
2021-12-09 22:39:55 +0800

    add nacos-starter

---
+++ nacos-starter/src/main/java/com/lx/nacos/configuration/NacosAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.lx.nacos.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: yuj
+ * @Date: 2021/10/14
+ * @Describe:
+ */
+@Configuration
+public class NacosAutoConfiguration {
+
+
+}
