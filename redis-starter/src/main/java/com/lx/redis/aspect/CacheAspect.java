eef1557e3abc75ec74cb67d7f6605aa576106b50
lishiyuan <lishiyuandage@gmail.com>
2022-03-18 10:50:35 +0800

    [bugfix]修复缓存与锁工具构建key错误

--- redis-starter/src/main/java/com/lx/redis/aspect/CacheAspect.java
+++ redis-starter/src/main/java/com/lx/redis/aspect/CacheAspect.java
@@ -48,18 +48,21 @@ public class CacheAspect {
         Object[] args = joinPoint.getArgs();
         for(int i=0; i<args.length; i++){
             Parameter parameter = parameters[i];
-            CacheKey cacheKey = parameter.getAnnotation(CacheKey.class);
             Object arg = args[i];
-            if(BeanUtils.isSimpleValueType(parameter.getType())){
-                builder.append( ":" + args.toString());
-                continue;
-            }
+
+            CacheKey cacheKey = parameter.getAnnotation(CacheKey.class);
+
             String field =cacheKey.field();
             if(StringUtils.isEmpty(field)){
                 field = cacheKey.value();
             }
             if(cacheKey !=null ){
-
+                // 如果是简单数据类型则直接获取值
+                if(BeanUtils.isSimpleValueType(parameter.getType())){
+                    builder.append( ":" + arg.toString());
+                    continue;
+                }
+                // 非简单类型则需要根据@CacheKey获取对象里面的field属性获取值
                 Field declaredField = parameter.getType().getDeclaredField(field);
                 declaredField.setAccessible(true);
                 Object fieldKey = declaredField.get(arg);
@@ -101,18 +104,19 @@ public class CacheAspect {
         Object[] args = joinPoint.getArgs();
         for(int i=0; i<args.length; i++){
             Parameter parameter = parameters[i];
-            CacheKey cacheKey = parameter.getAnnotation(CacheKey.class);
             Object arg = args[i];
-            if(BeanUtils.isSimpleValueType(parameter.getType())){
-                builder.append( ":" + args.toString());
-                continue;
-            }
+
+            CacheKey cacheKey = parameter.getAnnotation(CacheKey.class);
             String field =cacheKey.field();
             if(StringUtils.isEmpty(field)){
                 field = cacheKey.value();
             }
             if(cacheKey !=null ){
-
+                // 如果是简单数据类型则直接获取值
+                if(BeanUtils.isSimpleValueType(parameter.getType())){
+                    builder.append( ":" + arg.toString());
+                    continue;
+                }
                 Field declaredField = parameter.getType().getDeclaredField(field);
                 declaredField.setAccessible(true);
                 Object fieldKey = declaredField.get(arg);
